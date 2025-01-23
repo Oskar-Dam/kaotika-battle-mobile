21401d26926ea3d9aea446f28df9182881c67baa
Mortimer <62113737+Oskar-Dam@users.noreply.github.com>
2025-01-23 13:37:06 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
 import PWABadge from './PWABadge.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="kaotika-battle-mobile logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>kaotika-battle-mobile</h1>
       <PWABadge />
     </>
