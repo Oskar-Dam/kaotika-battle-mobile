f8340b8c8ef972b08d7bd1089a95aa3826ad70f3
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-24 14:02:19 +0100

    [KBM-3][UPDATE] Text font

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -16,7 +16,7 @@ const LoginScreen: React.FC<LoginScreenInterface> = ({ email, setEmail, setIsLog
     <div
       className="flex bg-black p-4 items-center justify-center h-screen w-screen"
       style={{ backgroundImage: 'url(/images/LoginBackground.png)', backgroundSize: '100% 100%' }}>
-      <div className="absolute top-[4%] w-full text-center">
+      <div className="absolute top-[4%] w-full text-center" style={{ fontFamily: 'Kaotika' }}>
         <h1 className="text-5xl text-white">Kaotika</h1>
         <h1 className="text-5xl text-white">The Final Battle</h1>
       </div>
@@ -26,8 +26,9 @@ const LoginScreen: React.FC<LoginScreenInterface> = ({ email, setEmail, setIsLog
             type="search"
             placeholder='Enter your email'
             id="default-input"
-            className="border border-yellow-600 text-yellow-600 text-sm rounded-lg  w-full p-2.5 bg-red-950 placeholder-yellow-600"
+            className="text-2xl border border-yellow-600 text-yellow-600 rounded-xs  w-full p-2.5 bg-red-950 placeholder-yellow-600"
             value={email}
+            style={{ fontFamily: 'Kaotika' }}
             onChange={handleEmailChange}></input>
         </div>
         <button
