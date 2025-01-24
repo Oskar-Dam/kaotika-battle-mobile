7b98e18205c0527349a47509c1e8344e1f21654e
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-24 13:54:25 +0100

    [KBM-3][UPDATE] LoginBackground

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -4,7 +4,7 @@ import { LoginScreenInterface } from '../interfaces/LoginScreenInterface';
 const LoginScreen: React.FC<LoginScreenInterface> = ({ email, setEmail, setIsLoggedIn }) => {
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value); 
+    setEmail(e.target.value);
   };
 
   const handleEnterBattle = () => {
@@ -13,12 +13,14 @@ const LoginScreen: React.FC<LoginScreenInterface> = ({ email, setEmail, setIsLog
   };
 
   return (
-    <div className="flex bg-black p-4 items-center justify-center h-screen w-screen">
+    <div
+      className="flex bg-black p-4 items-center justify-center h-screen w-screen"
+      style={{ backgroundImage: 'url(/images/LoginBackground.png)', backgroundSize: '100% 100%' }}>
       <div className="absolute top-[4%] w-full text-center">
         <h1 className="text-5xl text-white">Kaotika</h1>
         <h1 className="text-5xl text-white">The Final Battle</h1>
       </div>
-      <div className="flex flex-col items-center justify-center w-full max-w-[630px] h-[40%] border-4 border-white" style={{ backgroundImage: 'url(/images/LoginFrame.png)', backgroundSize: '100% 100%' }}>
+      <div className="flex flex-col items-center justify-center w-full max-w-[630px] h-[40%] border-0 border-white" style={{ backgroundImage: 'url(/images/LoginFrame.png)', backgroundSize: '100% 100%' }}>
         <div className="w-[80%]">
           <input
             type="search"
@@ -28,7 +30,7 @@ const LoginScreen: React.FC<LoginScreenInterface> = ({ email, setEmail, setIsLog
             value={email}
             onChange={handleEmailChange}></input>
         </div>
-        <button 
+        <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-[10%]"
           onClick={handleEnterBattle}>
           Enter the battle
