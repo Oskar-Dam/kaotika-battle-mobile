c2ef2de30b259533422cdaf2f92230c94b974c93
ionpazoos <jon.pazos@ikasle.aeg.eus>
2025-01-24 13:19:14 +0100

    [KBM-4][ADD] Implement waiting screen and button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
+import WaitingScreen from './components/Waiting.tsx'
 import PWABadge from './PWABadge.tsx'
 
 function App() {
+  const [showWaitingScreen, setShowWaitingScreen] = useState(false);
   return (
     <>
-   
+      {showWaitingScreen && <WaitingScreen />}
       <h1 className=' text-6xl text-center'>kaotika-battle-mobile</h1>
       <PWABadge />
     </>
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface SpinnerProps {
+    text?: string;
+}
+
+const Spinner: React.FC<SpinnerProps> = ({  text }) => {
+
+    return (
+        <div className="flex flex-col items-center justify-center space-y-4">
+            {/* Spinner */}
+            <div className="relative">
+                {/* Spinner con bordes de tonos derivados */}
+                <div
+                    className={`animate-spin rounded-full h-16 w-16 border-4 
+                        border-t-gray-200
+                        border-r-gray-400 
+                        border-b-gray-600
+                        border-l-gray-800`}
+                ></div>
+            </div>
+            {/* Texto */}
+            {text && <p className={`text-lg font-medium text-white`}>{text}</p>}
+        </div>
+    );
+};
+
+export default Spinner;
