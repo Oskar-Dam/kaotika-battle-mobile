62ebe725bb6446b1da1928967c6e54980dbd57c7
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-24 13:44:02 +0100

    [KBM-3][ADD] LoginScreen interface. LoginScreen props and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import PWABadge from './PWABadge.tsx'
 import LoginScreen from './pages/LoginScreen.tsx'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(false)
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+
+  isLoggedIn;
+  setIsLoggedIn;
+
   return (
     <>
-      {isLoggedIn ? <h1>Logged in</h1> : <LoginScreen/>}
+      {isLoggedIn ? <h1>Logged in</h1> : <LoginScreen email={email} setEmail={setEmail} setIsLoggedIn={setIsLoggedIn}/>}
       <PWABadge />
     </>
   )
--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent } from 'react';
+import { LoginScreenInterface } from '../interfaces/LoginScreenInterface';
 
-const LoginScreen: React.FC = () => {
-  // Definimos los estados para los campos de formulario
-  const [email, setEmail] = useState<string>('');
+const LoginScreen: React.FC<LoginScreenInterface> = ({ email, setEmail, setIsLoggedIn }) => {
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value); 
+  };
+
+  const handleEnterBattle = () => {
+    console.log('Email:', email);
+    setIsLoggedIn(true);
+  };
 
   return (
     <div className="flex bg-black p-4 items-center justify-center h-screen w-screen">
@@ -12,9 +20,17 @@ const LoginScreen: React.FC = () => {
       </div>
       <div className="flex flex-col items-center justify-center w-full max-w-[630px] h-[40%] border-4 border-white" style={{ backgroundImage: 'url(/images/LoginFrame.png)', backgroundSize: '100% 100%' }}>
         <div className="w-[80%]">
-          <input type="text 2xl" placeholder='Enter your email' id="default-input" className="border border-yellow-600 text-yellow-600 text-sm rounded-lg  w-full p-2.5 bg-red-950 placeholder-yellow-600"></input>
+          <input
+            type="search"
+            placeholder='Enter your email'
+            id="default-input"
+            className="border border-yellow-600 text-yellow-600 text-sm rounded-lg  w-full p-2.5 bg-red-950 placeholder-yellow-600"
+            value={email}
+            onChange={handleEmailChange}></input>
         </div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-[10%]">
+        <button 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-[10%]"
+          onClick={handleEnterBattle}>
           Enter the battle
         </button>
       </div>
