b817b0265c60d8a7a064b6305e5ed36cd562f608
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2025-01-24 14:01:50 +0100

    [DEVELOP][FIX] infinite loop error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+
+import BattleScreen from './pages/battleScreen.tsx'
 import { useState } from 'react'
 import WaitingScreen from './components/Waiting.tsx'
-// import BattleScreen from './pages/battleScreen.tsx'
 import PWABadge from './PWABadge.tsx'
-// import { potions } from './data/data.ts'
+import { potions } from './data/data.ts'
 
 function App() {
-  const [showWaitingScreen, setShowWaitingScreen] = useState(false);
-  setShowWaitingScreen(false); // delete this line  when the setusestate is used 
+  const [showWaitingScreen, setShowWaitingScreen] = useState(true);
+  console.log(setShowWaitingScreen);
   return (
     <>
       {showWaitingScreen && <WaitingScreen />}
+      <BattleScreen potions={potions} />
       <h1 className=' text-6xl text-center'>kaotika-battle-mobile</h1>
       <PWABadge />
     </>
