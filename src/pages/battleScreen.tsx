b3258262386997955df839738d01c83489c663db
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-27 09:50:38 +0100

    [KBM-17][ADD] ClassImage component

--- src/pages/battleScreen.tsx
+++ src/pages/battleScreen.tsx
@@ -5,6 +5,7 @@ import { Potion } from "../interfaces/Potion";
 import SelectOponentModal from "../components/SelectOponentModal";
 import Actions from "../components/Actions";
 import NickName from "../components/NickName";
+import ClassImage from "../components/ClassImage"; // Import the new component
 
 interface BattleScreenProps {
   potions: Potion[];
@@ -29,13 +30,11 @@ const BattleScreen: React.FC<BattleScreenProps> = ({
       <div
         className='w-full h-screen flex flex-col items-center justify-center'
         style={{ backgroundImage: 'url(/images/LoginBackground.png)', backgroundSize: 'cover' }}>
-        <div className="flex justify-center w-full h-[49%] border-2 border-blue-500">
-          {/* Contenido del primer componente */}
-        </div>
+        <ClassImage />
 
         <NickName />
 
-        <Actions potions={potions} openModal={openModal}/>
+        <Actions potions={potions} openModal={openModal} />
       </div>
       {isModalOpen && (
         <SelectOponentModal
