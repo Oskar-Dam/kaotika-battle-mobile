ef35102660109ff8f3e0f445864fecd7bcac0247
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-27 12:05:54 +0100

    [KBM-19][UPDATE] NickName component

--- src/components/NickName.tsx
+++ src/components/NickName.tsx
@@ -5,7 +5,7 @@ interface NickNameProps {
 
 const NickName: React.FC<NickNameProps> = ({nickname}) => {
   return (
-    <div className="w-full h-[2%] flex items-center justify-center">{nickname}
+    <div className="w-full h-[4%] flex items-center justify-center text-white">{nickname}
     </div>
   );
 };
