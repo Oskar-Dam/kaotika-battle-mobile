a1683d91cad2cd9b8d41bac6f7aea4799b31cc45
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-27 09:49:36 +0100

    [KBM-17][ADD] NickName and Actions components

---
+++ src/components/NickName.tsx
@@ -0,0 +1,8 @@
+const NickName: React.FC = () => {
+  return (
+    <div className="w-full h-[2%] flex items-center justify-center">
+    </div>
+  );
+};
+
+export default NickName;
--- src/pages/battleScreen.tsx
+++ src/pages/battleScreen.tsx
@@ -3,6 +3,8 @@ import AttackButton from "../components/AttackButton";
 import PotionContainer from "../components/PotionContainer";
 import { Potion } from "../interfaces/Potion";
 import SelectOponentModal from "../components/SelectOponentModal";
+import Actions from "../components/Actions";
+import NickName from "../components/NickName";
 
 interface BattleScreenProps {
   potions: Potion[];
@@ -11,30 +13,29 @@ interface BattleScreenProps {
 const BattleScreen: React.FC<BattleScreenProps> = ({
   potions,
 }) => {
-   
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
-      setIsModalOpen(true);
+    setIsModalOpen(true);
   };
 
   const closeModal = () => {
-      setIsModalOpen(false);
+    setIsModalOpen(false);
   };
 
   return (
     <>
-      <div className='w-full flex flex-col items-center justify-center'>
-        <AttackButton
-          text={'Attack'}
-          onClick={openModal}
-        />
-        <div className='w-full flex items-center justify-center m-[10%]'>
-          <PotionContainer
-            potions={potions}
-          />
+      <div
+        className='w-full h-screen flex flex-col items-center justify-center'
+        style={{ backgroundImage: 'url(/images/LoginBackground.png)', backgroundSize: 'cover' }}>
+        <div className="flex justify-center w-full h-[49%] border-2 border-blue-500">
+          {/* Contenido del primer componente */}
         </div>
 
+        <NickName />
+
+        <Actions potions={potions} openModal={openModal}/>
       </div>
       {isModalOpen && (
         <SelectOponentModal
