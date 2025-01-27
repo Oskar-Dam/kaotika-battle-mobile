2e6e4b0e092d97f2b3eaef36d65a54f3f8b58731
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-27 10:18:16 +0100

    [KBM-18][UPDATE] PlayerInterface

---
+++ src/interfaces/PlayerInterface.tsx
@@ -0,0 +1,59 @@
+interface Attributes {
+  intelligence: number;
+  dexterity: number;
+  insanity: number;
+  charisma: number;
+  constitution: number;
+  strength: number;
+}
+
+interface Equipment {
+  helmet: object;
+  weapon: object;
+  armor: object;
+  shield: object;
+  artifact: object;
+  boot: object;
+  ring: object;
+  antidote_potion: object;
+  healing_potion: object;
+  enhancer_potion: object;
+}
+
+interface Inventory {
+  antidote_potions: object[];
+  healing_potions: object[];
+  enhancer_potions: object[];
+}
+
+interface Status {
+  ethaziumCurse: boolean;
+  common_diseases: object[];
+  tired: boolean;
+}
+
+interface Profile {
+  _id: string;
+  name: string;
+  description: string;
+  image: string;
+  attributes: any[];
+}
+
+interface PlayerInterface {
+  attributes: Attributes;
+  equipment: Equipment;
+  inventory: Inventory;
+  status: Status;
+  _id: string;
+  name: string;
+  nickname: string;
+  email: string;
+  avatar: string;
+  level: number;
+  profile: Profile;
+  role: string;
+  resistance: number;
+}
+
+export default PlayerInterface;
