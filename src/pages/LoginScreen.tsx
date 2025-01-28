f542722aaebd8a54231a5b8944346e5f1c2d6bd2
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-28 08:37:06 +0100

    [KBM-27][ADD] Spinner when making first petition

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,16 +1,19 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { LoginScreenInterface } from '../interfaces/LoginScreenInterface';
-
+import Spinner from '../components/Spinner';
 import socket from '../sockets/socket';
 import { SOCKET_EVENTS } from '../sockets/events';
 
 const LoginScreen: React.FC<LoginScreenInterface> = ({ email, setEmail, setIsLoggedIn, setPlayer }) => {
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
 
   const handleEnterBattle = async () => {
+    setIsLoading(true);
     console.log('Email:', email);
     try {
       const response = await fetch(`https://kaotika-battle-server.onrender.com/api/player/${email}`);
@@ -29,6 +32,7 @@ const LoginScreen: React.FC<LoginScreenInterface> = ({ email, setEmail, setIsLog
       socketId: socket.id
     });
     setIsLoggedIn(true);
+    setIsLoading(false);
       setPlayer(playerData.data);
     } catch (error) {
       console.error('Fetch error:', error);
@@ -39,6 +43,11 @@ const LoginScreen: React.FC<LoginScreenInterface> = ({ email, setEmail, setIsLog
     <div
       className="flex bg-black p-4 items-center justify-center h-screen w-screen"
       style={{ backgroundImage: 'url(/images/LoginBackground.png)', backgroundSize: '100% 100%' }}>
+      {isLoading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/80 z-50">
+          <Spinner text={'Retrieving player from database, please wait...'} />
+        </div>
+      )}
       <div className="absolute top-[4%] w-full text-center" style={{ fontFamily: 'Kaotika' }}>
         <h1 className="text-5xl text-white">Kaotika</h1>
         <h1 className="text-5xl text-white">The Final Battle</h1>
