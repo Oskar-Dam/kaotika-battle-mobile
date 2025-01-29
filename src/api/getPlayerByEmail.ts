ea58c02bf45e7ea47aafbbef2fd3104c13eb0f54
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-29 09:31:38 +0100

    [KBM-42][UPDATE] Email petition and error handling

---
+++ src/api/getPlayerByEmail.ts
@@ -0,0 +1,14 @@
+export const getPlayerByEmail = async (email: string) => {
+  const response = await fetch(`https://kaotika-battle-server.onrender.com/api/player/${email}`);
+
+  if (!response.ok) {
+    if (response.status === 500) {
+      throw new Error('Email not found');
+    } else {
+      throw new Error('Failed to fetch player data');
+    }
+  }
+
+  const playerData = await response.json();
+  return playerData;
+};
