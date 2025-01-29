e76aa5c41de5a8cab26eb2f74373c2f4a8f2954a
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-29 08:26:50 +0100

    [KBM-33][ADD] Potion's name to the modal

--- src/components/PotionModal.tsx
+++ src/components/PotionModal.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Potion } from '../interfaces/Potion';
 
 interface PotionModalProps {
+  potion: Potion
   closeModal: () => void;
 }
 
 const PotionModal: React.FC<PotionModalProps> = ({
+  potion,
   closeModal
 }) => {
   return (
     <div className="w-full rounded-3xl fixed inset-0 z-50 flex items-center justify-center">
-      <div className="w-[90%] h-[90%] rounded-3xl inset-0 z-50 flex items-center justify-center bg-black/90 ">
-        <button
-          onClick={closeModal}
-          className="absolute top-[5%] right-[10%] text-black rounded-full text-xl font-bold mt-8 bg-white/70 px-3 py-1 hover:text-zinc-800/80"
-        >
-          X
-        </button>
+      <div className="w-[90%] h-[40%] rounded-3xl inset-0 z-50 flex flex-col items-center justify-center bg-black/90 ">
+        <div className="w-full flex justify-end p-4">
+          <button
+            onClick={closeModal}
+            className="text-black rounded-full text-xl font-bold bg-white/70 px-3 py-1 hover:text-zinc-800/80"
+          >
+            X
+          </button>
+        </div>
+        <h1 className="text-white text-2xl font-bold">{potion.name}</h1>
       </div>
     </div>
   );
