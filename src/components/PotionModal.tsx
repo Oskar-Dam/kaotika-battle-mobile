0419ae7a509d60a3f60eda5cc872e87798a6a384
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-29 08:52:34 +0100

    [KBM-33][UPDATE] PotionModal

--- src/components/PotionModal.tsx
+++ src/components/PotionModal.tsx
@@ -11,17 +11,24 @@ const PotionModal: React.FC<PotionModalProps> = ({
   closeModal
 }) => {
   return (
-    <div className="w-full rounded-3xl fixed inset-0 z-50 flex items-center justify-center">
-      <div className="w-[90%] h-[40%] rounded-3xl inset-0 z-50 flex flex-col items-center justify-center bg-black/90 ">
-        <div className="w-full flex justify-end p-4">
+    <div className="w-full rounded-3xl fixed inset-0 z-50 flex items-end justify-center pb-[15%]">
+      <div className="w-[75%] h-[35%] rounded-3xl z-50 flex flex-col items-center justify-center bg-black/90 relative border-2 border-white">
+        <div className="absolute top-2 right-2">
           <button
             onClick={closeModal}
             className="text-black rounded-full text-xl font-bold bg-white/70 px-3 py-1 hover:text-zinc-800/80"
+            aria-label="Close modal"
           >
             X
           </button>
         </div>
-        <h1 className="text-white text-2xl font-bold">{potion.name}</h1>
+        <h1 className="text-white text-2xl font-bold my-[2%]">{potion.name}</h1>
+        <h2 className="text-white text-xl capitalize my-[1%]">Type: {potion.type}</h2>
+        <h2 className="text-white text-xl capitalize my-[2%]">Effect: {potion.effects.primary.attribute}</h2>
+
+        <button className="mt-4 w-[60%]">
+          <img src="/images/ATTACK_BUTTON.png" alt="Attack Button" />
+        </button>
       </div>
     </div>
   );
