0f4fd65151a6b0d183b48552642345bad4da16d4
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-29 12:37:46 +0100

    [KBM-43][UPDATE] BlockedScreen

--- src/components/BlockedScreen.tsx
+++ src/components/BlockedScreen.tsx
@@ -11,13 +11,28 @@ const BlockedScreen: React.FC = () => {
   }, []);
 
   return (
-    <div
-      className="absolute top-0 left-0 w-[100%] h-[100%] flex items-center justify-center z-1"
-      style={{ backgroundImage: 'url(/images/blocked-chains.png)', backgroundSize: '120% 100%', backgroundPosition: '45% 0%', opacity: 1 }}>
-      <h1 className="text-white text-4xl" style={{ fontFamily: 'Kaotika' }}>
-        Please wait for your turn<span style={{ visibility: 'hidden' }}>...</span>
-        <span style={{ position: 'absolute', marginLeft: '-0.85rem' }}>{dots}</span>
-      </h1>
+    <div className="absolute top-0 left-0 w-[100%] h-[100%] z-1">
+      <div
+        className="absolute top-0 left-0 w-[100%] h-[100%]"
+        style={{
+          backgroundColor: 'black',
+          opacity: 0.8
+        }}></div>
+      <div className="absolute top-0 left-0 w-[100%] h-[100%]"
+        style={{
+          backgroundImage: 'url(/images/blocked-chains-no-bg.png)',
+          backgroundSize: '120% 100%', backgroundPosition: '45% 0%',
+          opacity: 1,
+          filter: 'brightness(80%)'
+        }}></div>
+      <div className="absolute top-[7%] left-0 w-[100%] h-[100%] flex items-start justify-center pt-10">
+        <h1
+          className="text-white text-4xl"
+          style={{ fontFamily: 'Kaotika' }}>
+          Waiting for your turn<span style={{ visibility: 'hidden' }}>...</span>
+          <span style={{ position: 'absolute', marginLeft: '-0.85rem' }}>{dots}</span>
+        </h1>
+      </div>
     </div>
   );
 };
