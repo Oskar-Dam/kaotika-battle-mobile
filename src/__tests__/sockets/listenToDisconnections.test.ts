93c558c0a82c291a6d81111136d64b11b646b5bc
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2025-02-11 13:12:02 +0100

    [KBM-141][ADD] test of the listenToDisconnections socket

---
+++ src/__tests__/sockets/listenToDisconnections.test.ts
@@ -0,0 +1,35 @@
+import { SOCKET_EVENTS } from '../../sockets/events';
+import socket from '../../sockets/socket';
+import { listenToDisconnections } from '../../sockets/socketListeners';
+
+beforeAll(() => {
+  jest.spyOn(console, 'log').mockImplementation(() => {}); // Silence console logs
+  jest.spyOn(console, 'error').mockImplementation(() => {}); // Silence console errors
+  jest.spyOn(console, 'warn').mockImplementation(() => {}); // Silence console warnings
+});
+
+jest.mock('../../sockets/socket', () => ({
+  on: jest.fn(),
+  emit: jest.fn(),
+}));
+
+describe('Socket Listeners', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Cambia de turno correctamente y emite evento de selección de jugador', () => {
+    const setdisconnection = jest.fn();
+  
+    listenToDisconnections(setdisconnection);
+  
+    const callback = (socket.on as jest.Mock).mock.calls.find(([event]) => event === SOCKET_EVENTS.DISCONNECT)?.[1];
+    
+    callback();
+
+    console.log('Llamadas a setIsMyTurn:', setdisconnection.mock.calls);
+  
+    expect(setdisconnection).toHaveBeenCalledWith(true);
+  });
+});
+
