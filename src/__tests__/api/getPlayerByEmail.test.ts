9c1a666f56082481dcf334e4a3fa4d3b05694ad7
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-02-11 12:18:09 +0100

    [KBM-103][ADD] The function getPlayerByEmail

---
+++ src/__tests__/api/getPlayerByEmail.test.ts
@@ -0,0 +1,32 @@
+import { ONLINE_USERS_MOCK } from '../../__mocks__/mockPlayers';
+import { getPlayerByEmail } from './../../api/player'; // Adjust the import as per your file structure
+
+
+beforeAll(() => {
+  jest.spyOn(console, 'log').mockImplementation(() => { }); // Silence console logs
+  jest.spyOn(console, 'error').mockImplementation(() => { }); // Silence console errors
+  jest.spyOn(console, 'warn').mockImplementation(() => { }); // Silence console warnings
+});
+
+beforeEach(() => {
+  global.fetch = jest.fn();
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('getPlayerByEmail', () => {
+
+  it('it should return player data of -aitor.mendiburu@ikasle.aeg.eus- when is successfully', async () => {
+    const mockData = { data: ONLINE_USERS_MOCK[0] };
+    (global.fetch as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockData),
+    });
+
+    const email = 'aitor.mendiburu@ikasle.aeg.eus';
+    const resultPlayer = await getPlayerByEmail(email);
+    expect(resultPlayer.email).toBe(email);
+  });
+});
