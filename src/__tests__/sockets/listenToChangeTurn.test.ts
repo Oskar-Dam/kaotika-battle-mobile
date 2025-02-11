33fb145cb4381737caa4f7ed683d5f7dbeae5a19
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2025-02-11 11:41:31 +0100

    [KBM-139][ADD] test of listenToInsufficientPlayers socket

--- src/__tests__/sockets/listenToChangeTurn.test.ts
+++ src/__tests__/sockets/listenToChangeTurn.test.ts
@@ -1,10 +1,9 @@
-import { listenToChangeTurn } from '../../sockets/socketListeners';
 import { SOCKET_EVENTS } from '../../sockets/events';
 import socket from '../../sockets/socket';
-import { mockDividedPlayers } from '../../__mocks__/mockPlayers';
+import { listenToInsufficientPlayers } from '../../sockets/socketListeners';
 
 beforeAll(() => {
-
+  jest.spyOn(console, 'log').mockImplementation(() => {}); // Silence console logs
   jest.spyOn(console, 'error').mockImplementation(() => {}); // Silence console errors
   jest.spyOn(console, 'warn').mockImplementation(() => {}); // Silence console warnings
 });
@@ -20,27 +19,17 @@ describe('Socket Listeners', () => {
   });
 
   it('Cambia de turno correctamente y emite evento de selección de jugador', () => {
-    const setIsMyTurn = jest.fn();
-    const setSelectedPlayerIndex = jest.fn();
+    const setInsufficientPlayers = jest.fn();
   
-    const dravocarPlayers = mockDividedPlayers.dravocar;
-    const kaotikaPlayers = mockDividedPlayers.kaotika;
-    const mockPlayer = mockDividedPlayers.kaotika[1];
+    listenToInsufficientPlayers(setInsufficientPlayers);
   
-    listenToChangeTurn(setIsMyTurn, mockPlayer, kaotikaPlayers, dravocarPlayers, setSelectedPlayerIndex);
-  
-    const callback = (socket.on as jest.Mock).mock.calls.find(([event]) => event === SOCKET_EVENTS.TURN_CHANGE)?.[1];
-    console.log(setIsMyTurn);
-    
-    
+    const callback = (socket.on as jest.Mock).mock.calls.find(([event]) => event === SOCKET_EVENTS.INSUFFICIENT_PLAYERS)?.[1];
+        
     callback();
 
-    console.log('Llamadas a setIsMyTurn:', setIsMyTurn.mock.calls);
+    console.log('Llamadas a setIsMyTurn:', setInsufficientPlayers.mock.calls);
   
-    expect(setIsMyTurn).toHaveBeenCalledWith(true);
-    expect(setSelectedPlayerIndex).toHaveBeenCalledWith(dravocarPlayers.length);
-    expect(setSelectedPlayerIndex).toHaveBeenCalledWith(1);
-    expect(socket.emit).toHaveBeenCalledWith('mobile-setSelectedPlayer', dravocarPlayers[0]._id);
+    expect(setInsufficientPlayers).toHaveBeenCalledWith(true);
   });
-  
 });
+ 
