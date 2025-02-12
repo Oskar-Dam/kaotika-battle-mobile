ef961d8bc16eae819e93ac4e00774c650f1c655c
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-12 12:32:47 +0100

    [KBM-147][UPDATE] Separate WelcomeTexts to another component

--- src/components/MortimerScreen.tsx
+++ src/components/MortimerScreen.tsx
@@ -3,6 +3,7 @@ import GameStartButton from './GameStartButton';
 import BattleTypeDropdown from './BattleTypeDropdown';
 import { SOCKET_EVENTS } from '../sockets/events';
 import socket from '../sockets/socket';
+import WelcomeTexts from './WelcomeTexts';
 
 const MortimerScreen: React.FC = () => {
   const [selection, setSelection] = useState<string>('CHOOSE BATTLE TYPE');
@@ -14,14 +15,7 @@ const MortimerScreen: React.FC = () => {
 
   return (
     <div className='flex flex-col h-screen'>
-      <div className='flex flex-col justify-start mt-48'>
-        <div className='flex justify-center'>
-          <h1 className='text-center text-5xl text-white mb-10'>Welcome, Mortimer</h1>
-        </div>
-        <div className='flex justify-center'>
-          <h2 className='text-center text-3xl text-white'>What battle looms ahead?</h2>
-        </div>
-      </div>
+      <WelcomeTexts />
       <div className='flex flex-col justify-center mt-20'>
         <div className='flex justify-center'>
           <BattleTypeDropdown
