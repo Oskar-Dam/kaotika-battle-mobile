a70228c82b4d337c444def07e78640a1799f5c6d
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-02-12 08:27:11 +0100

    [KBM-103][ADD] More test to the function getPlayerByEmail

--- src/__tests__/api/getPlayerByEmail.test.ts
+++ src/__tests__/api/getPlayerByEmail.test.ts
@@ -24,9 +24,36 @@ describe('getPlayerByEmail', () => {
       ok: true,
       json: jest.fn().mockResolvedValue(mockData),
     });
-
     const email = 'aitor.mendiburu@ikasle.aeg.eus';
     const resultPlayer = await getPlayerByEmail(email);
     expect(resultPlayer.email).toBe(email);
   });
+
+  it('it should throw an error when the email -mikel161211231@ikasle.aeg.eus- is not found [status error 500', async () => {
+    (global.fetch as jest.Mock).mockResolvedValue({
+      ok: false,
+      status: 500,
+    });
+    const email = 'mikel161211231@ikasle.aeg.eus';
+    await expect(getPlayerByEmail(email)).rejects.toThrow('Email not found');
+  });
+
+  it('it should throw an error when the game already started [status error 403]', async () => {
+    (global.fetch as jest.Mock).mockResolvedValue({
+      ok: false,
+      status: 403,
+    });
+    const email = 'aitor.mendiburu@ikasle.aeg.eus';
+    await expect(getPlayerByEmail(email)).rejects.toThrow('Game already started');
+  });
+  
+  it('it should throw an error when the server failed to fetch the player data of the email -aitor.mendiburu@ikasle.aeg.eus-', async () => {
+    (global.fetch as jest.Mock).mockResolvedValue({
+      ok: false,
+      status: 400,
+    });
+    const email = 'aitor.mendiburu@ikasle.aeg.eus';
+    await expect(getPlayerByEmail(email)).rejects.toThrow('Failed to fetch player data');
+  });
+
 });
