77be2f90f4955b266a2a25ed45fb8bdeacfad410
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-02-12 12:19:24 +0100

    [KBM-154][ADD]

--- src/components/GameEndingModal.tsx
+++ src/components/GameEndingModal.tsx
@@ -16,10 +16,16 @@ const GameEndingModal: React.FC<GameEndingModalProps> = ({ setPlayer, setIsLogge
     setEmail('');
   };
 
+  const imgUrl: string = (winner === 'kaotika') ? 'url(/images/kaotikaWinner.webp)' :
+    (winner === 'dravocar') ? 'url(/images/dravokarWinner.webp)' :
+      'url(/images/login-background.webp)';
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black z-51">
-      <div className="flex flex-col bg-black p-8 rounded shadow-lg justify-center text-center w-full h-full items-center">
+
+      <div
+        className="flex flex-col bg-black p-8 rounded shadow-lg justify-center text-center w-full h-full items-center"
+        style={{ backgroundImage: imgUrl, backgroundSize: 'w-screen h-screen' }}>
         <h2 className="text-6xl font-bold mb-4 text-white">Game Over</h2>
         <p className="mb-4 text-4xl text-white">Winner: {winner}</p>  {/* Show who won */}
         <button
