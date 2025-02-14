f1970b6d0d30c22931b68309b8f578ba7033fa96
Unai <reikrb@gmail.com>
2025-02-14 09:02:13 +0100

    [KBM-149][UPDATE] listenToRemovePlayer tests

--- src/__tests__/api/sockets/listeners/listenToRemovePlayer.test.ts
+++ src/__tests__/api/sockets/listeners/listenToRemovePlayer.test.ts
@@ -15,11 +15,11 @@ const setKaotikaPlayers = jest.fn();
 const setDravokarPlayers = jest.fn();
 const setUserDead = jest.fn();
 
-// beforeAll(() => {
-//   jest.spyOn(console, 'error').mockImplementation(() => { }); // Silence console errors
-//   jest.spyOn(console, 'warn').mockImplementation(() => { }); // Silence console warnings
-//   jest.spyOn(console, 'log').mockImplementation(() => { }); // Silence console logs
-// });
+beforeAll(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => { }); // Silence console errors
+  jest.spyOn(console, 'warn').mockImplementation(() => { }); // Silence console warnings
+  jest.spyOn(console, 'log').mockImplementation(() => { }); // Silence console logs
+});
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -77,54 +77,54 @@ describe('listenToRemovePlayer', () => {
     expect(newKaotikaPlayers.length).toBe(1);
   });
 
-  // it('Should remove the user in dravokar array when SOCKET_EVENTS.REMOVE_PLAYER is event is received', () => {
-  //   const { kaotika, dravokar }: { kaotika: Player[], dravokar: Player[] } = mockDividedPlayers;
-  //   const player: Player = ONLINE_USERS_MOCK[0];
-  //   const playerId: string = player._id;
+  it('Should remove the user in dravokar array when SOCKET_EVENTS.REMOVE_PLAYER is event is received', () => {
+    const { kaotika, dravokar }: { kaotika: Player[], dravokar: Player[] } = mockDividedPlayers;
+    const player: Player = ONLINE_USERS_MOCK[2];
+    const playerId: string = player._id;
 
 
-  //   listenToRemovePlayer(setKaotikaPlayers, setDravokarPlayers, kaotika, dravokar, setUserDead, player);
+    listenToRemovePlayer(setKaotikaPlayers, setDravokarPlayers, kaotika, dravokar, setUserDead, player);
 
-  //   const callback = (socket.on as jest.Mock).mock.calls.find(([event]) => event === SOCKET_EVENTS.REMOVE_PLAYER)?.[1];
-  //   callback(playerId);
+    const callback = (socket.on as jest.Mock).mock.calls.find(([event]) => event === SOCKET_EVENTS.REMOVE_PLAYER)?.[1];
+    callback(playerId);
 
-  //   // Value of the data used on the first call of the mocked functions
-  //   // const newDravokarPlayers = setDravokarPlayers.mock.calls[0][0];
-  //   console.log('showing mock calls: ',setDravokarPlayers.mock);
+    // Value of the data used on the first call of the mocked functions
+    const newDravokarPlayers = setDravokarPlayers.mock.calls[0][0];
+    console.log('showing mock calls: ',setDravokarPlayers.mock.calls);
     
-  //   expect(setKaotikaPlayers).toHaveBeenCalledTimes(0);
-  //   // expect(setDravokarPlayers).toHaveBeenCalledTimes(1);
+    expect(setKaotikaPlayers).toHaveBeenCalledTimes(0);
+    expect(setDravokarPlayers).toHaveBeenCalledTimes(1);
 
-  //   // expect(setDravokarPlayers).toHaveBeenCalledWith(mockDividedPlayers.dravokar);
+    expect(setDravokarPlayers).toHaveBeenCalledWith(mockDividedPlayers.dravokar);
 
-  //   // expect(newDravokarPlayers).toEqual([]);
+    expect(newDravokarPlayers).toEqual([]);
 
-  //   // expect(newDravokarPlayers.length).toBe(0);
-  // });
+    expect(newDravokarPlayers.length).toBe(0);
+  });
 
-  // it('Should remove the user in kaotika array when SOCKET_EVENTS.REMOVE_PLAYER is event is received', () => {
-  //   const { kaotika, dravokar }: { kaotika: Player[], dravokar: Player[] } = mockDividedPlayers;
-  //   const player: Player = ONLINE_USERS_MOCK[1];
-  //   const playerId: string = player._id;
-  //   const setKaotikaPlayers = jest.fn();
-  //   const setDravokarPlayers = jest.fn();
-  //   const setUserDead = jest.fn();
+  it('Should remove the user in kaotika array when SOCKET_EVENTS.REMOVE_PLAYER is event is received', () => {
+    const { kaotika, dravokar }: { kaotika: Player[], dravokar: Player[] } = mockDividedPlayers;
+    const player: Player = ONLINE_USERS_MOCK[3];
+    const playerId: string = player._id;
+    const setKaotikaPlayers = jest.fn();
+    const setDravokarPlayers = jest.fn();
+    const setUserDead = jest.fn();
 
-  //   listenToRemovePlayer(setKaotikaPlayers, setDravokarPlayers, kaotika, dravokar, setUserDead, player);
+    listenToRemovePlayer(setKaotikaPlayers, setDravokarPlayers, kaotika, dravokar, setUserDead, player);
 
-  //   const callback = (socket.on as jest.Mock).mock.calls.find(([event]) => event === SOCKET_EVENTS.REMOVE_PLAYER)?.[1];
-  //   callback(playerId);
+    const callback = (socket.on as jest.Mock).mock.calls.find(([event]) => event === SOCKET_EVENTS.REMOVE_PLAYER)?.[1];
+    callback(playerId);
 
-  //   //Value of the data used on the first call of the mocked functions
-  //   const newKaotikaPlayers = setKaotikaPlayers.mock.calls[0][0];
+    //Value of the data used on the first call of the mocked functions
+    const newKaotikaPlayers = setKaotikaPlayers.mock.calls[0][0];
 
-  //   expect(setKaotikaPlayers).toHaveBeenCalledTimes(1);
-  //   expect(setDravokarPlayers).toHaveBeenCalledTimes(0);
+    expect(setKaotikaPlayers).toHaveBeenCalledTimes(1);
+    expect(setDravokarPlayers).toHaveBeenCalledTimes(0);
 
-  //   expect(setKaotikaPlayers).toHaveBeenCalledWith(mockDividedPlayers.kaotika);
+    expect(setKaotikaPlayers).toHaveBeenCalledWith(mockDividedPlayers.kaotika);
 
-  //   expect(newKaotikaPlayers).toEqual([]);
+    expect(newKaotikaPlayers).toEqual([]);
 
-  //   expect(newKaotikaPlayers.length).toBe(0);
-  // });
+    expect(newKaotikaPlayers.length).toBe(0);
+  });
 });
