b4351ba4896e0e5325edbc85d43696d2d7c79a9f
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-14 09:49:57 +0100

    [KBM-170][ADD] listenToGameReset call in BattleScreen

--- src/pages/BattleScreen.tsx
+++ src/pages/BattleScreen.tsx
@@ -14,7 +14,7 @@ import { Player } from '../interfaces/Player';
 
 import { Potion } from '../interfaces/Potion';
 import socket from '../sockets/socket';
-import { clearListenToServerEventsBattleScreen, listenToChangeTurn, listenToGameEnded, listenToRemovePlayer, listenToServerEventsBattleScreen, listenToUpdatePlayer } from '../sockets/socketListeners';
+import { clearListenToServerEventsBattleScreen, listenToChangeTurn, listenToGameEnded, listenToGameReset, listenToRemovePlayer, listenToServerEventsBattleScreen, listenToUpdatePlayer } from '../sockets/socketListeners';
 import DeadScreen from './DeadScreen';
 import { SOCKET_EMIT_EVENTS } from '../sockets/events';
 interface BattleScreenProps {
@@ -60,6 +60,7 @@ const BattleScreen: React.FC<BattleScreenProps> = ({
     listenToRemovePlayer(setKaotikaPlayers, setDravokarPlayers, kaotikaPlayers, dravokarPlayers, setUserDead, player);
     listenToChangeTurn(setIsMyTurn, player, kaotikaPlayers, dravokarPlayers, setSelectedPlayerIndex);
     listenToGameEnded(setGameEnded, setWinner); 
+    listenToGameReset(setGameEnded, setIsMyTurn, setIsLoggedIn, setEmail, setPlayer, setKaotikaPlayers, setDravokarPlayers);
 
     console.log('KAOTIKA PLAYERS: ', kaotikaPlayers);
     console.log('DRAVOKAR PLAYERS: ', dravokarPlayers);
