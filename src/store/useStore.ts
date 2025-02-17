463a9f825931e3272715e36fa0ba5c9e88ee531d
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-17 12:38:35 +0100

    [KBM-200][ADD] Zustand library and config

---
+++ src/store/useStore.ts
@@ -0,0 +1,35 @@
+import { create } from 'zustand';
+import { Player } from '../interfaces/Player';
+
+interface GameState {
+  isLoggedIn: boolean;
+  email: string;
+  player: Player | null;
+  isMyTurn: boolean;
+  isDisconnected: boolean;
+  permanentlyDisconnected: boolean;
+  setIsLoggedIn: (loggedIn: boolean) => void;
+  setEmail: (email: string) => void;
+  setPlayer: (player: Player | null) => void;
+  setIsMyTurn: (turn: boolean) => void;
+  setIsDisconnected: (disconnected: boolean) => void;
+  setPermanentlyDisconnected: (disconnected: boolean) => void;
+}
+
+const useGameStore = create<GameState>((set) => ({
+  isLoggedIn: false,
+  email: '',
+  player: null,
+  isMyTurn: true,
+  isDisconnected: false,
+  permanentlyDisconnected: false,
+  
+  setIsLoggedIn: (loggedIn) => set({ isLoggedIn: loggedIn }),
+  setEmail: (email) => set({ email }),
+  setPlayer: (player) => set({ player }),
+  setIsMyTurn: (turn) => set({ isMyTurn: turn }),
+  setIsDisconnected: (disconnected) => set({ isDisconnected: disconnected }),
+  setPermanentlyDisconnected: (disconnected) => set({ permanentlyDisconnected: disconnected }),
+}));
+
+export default useGameStore;
