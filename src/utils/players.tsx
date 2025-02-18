71b6681b06bcf0a20ba0b07ee1a345cd8075d2f2
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2025-02-18 21:19:34 +0100

    [KBM-218][FIX] update player test

--- src/utils/players.tsx
+++
@@ -1,8 +0,0 @@
-import { Modifier } from '../interfaces/Modifier';
-import { Player } from '../interfaces/Player';
-
-export const updateSessionPlayerAttributesIfIdMatches = (updatedPlayer: { _id: string, attributes: Modifier, totalDamage: number, isBetrayer: boolean }, setPlayer: (players: Player) => void, sessionPlayer: Player): void => {
-  if (updatedPlayer._id === sessionPlayer._id) {
-    setPlayer({ ...sessionPlayer, attributes: updatedPlayer.attributes });
-  }
-};
