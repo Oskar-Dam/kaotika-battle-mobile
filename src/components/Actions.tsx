802a7758ded889f4ba1fcda2ada4a7275fdcc5ff
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-18 11:02:18 +0100

    [KBM-211][UPDATE] AttackButton component and test. PotionContainer component and test

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { SOCKET_EMIT_EVENTS } from '../sockets/events';
-import socket from '../sockets/socket';
 import AttackButton from './AttackButton';
 import EndGameButton from './EndGameButton';
 import PotionContainer from './PotionContainer';
@@ -14,9 +12,6 @@ interface ActionsProps {
 
 const Actions: React.FC<ActionsProps> = ({ potions, openModal }) => {
   const {
-    isMyTurn,
-    setIsMyTurn,
-    selectedPlayer,
     player,
   } = useStore();
 
@@ -24,16 +19,7 @@ const Actions: React.FC<ActionsProps> = ({ potions, openModal }) => {
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-[49%] border-0 border-yellow-400">
-      <AttackButton
-        player={player}
-        onClick={() => {
-          console.log('Attacking ', selectedPlayer?._id);
-          socket.emit(SOCKET_EMIT_EVENTS.ATTACK, selectedPlayer?._id);
-          setIsMyTurn(false);
-        }}
-        isMyTurn={isMyTurn}
-        selectedPlayer={selectedPlayer}
-      />
+      <AttackButton/>
       {(player?.role === 'mortimer') && (
         <div className='w-full flex items-center place-self-center justify-center mt-[10%]'>
           <EndGameButton classNameCss={classNameEndGameButton} />
