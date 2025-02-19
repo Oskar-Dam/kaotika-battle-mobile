46b963e17fdfef2314ca8a45aa742d896b7ac8f8
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-19 10:05:36 +0100

    [KBM-225][UPDATE] MainFrame component

--- src/components/MainFrame.tsx
+++ src/components/MainFrame.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+// import React from 'react';
+// import StaminaBar from './StaminaBar';
+// import HitPointsBar from './HitPointsBar';
+// import Avatar from './Avatar';
+// import useStore from '../store/useStore';
 
-interface MainFrameProps {
-  frameBackground: string;
-}
+// interface MainFrameProps {
+//   frameBackground: string;
+// }
 
-const MainFrame: React.FC<MainFrameProps> = ({frameBackground}) => {
+// const MainFrame: React.FC<MainFrameProps> = ({frameBackground}) => {
 
-  return(
-    <div
-      className='w-screen h-screen flex flex-col items-center justify-center top-0 z-20'
-      style={{ backgroundImage: frameBackground, backgroundSize: '100% 100%' }}
-      data-testid="battle-screen"
-    />
-  );
-};
+//   const {player, selectedPlayer, openModal} = useStore();
 
-export default MainFrame;
+//   return(
+//     <div
+//       className='w-screen h-screen flex flex-col items-center justify-center top-0 z-20'
+//       style={{ backgroundImage: frameBackground, backgroundSize: '100% 100%' }}
+//       data-testid="battle-screen"
+//     >
+//       <StaminaBar/>
+//       <HitPointsBar/>
+
+//       {/* AVATAR */}
+//       <Avatar
+//         avatar={player?.avatar}
+//         faction={player?.isBetrayer}/>
+
+//       {/* CAROUSEL CONTAINER */}
+//       <CarouselContainer/>
+        
+//       {/* SELECTED PLAYER NICK */}
+//       <NickName nickname={selectedPlayer?.nickname} />
+
+//       {/* ACTION BUTTONS */}
+//       <Actions
+//         openModal={openModal}
+//       />
+//     </div>
+//   );
+// };
+// export default MainFrame;
