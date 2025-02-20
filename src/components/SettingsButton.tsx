99149f8b1c581bda7cfe3c3c48c108287d75ca06
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-02-20 09:47:03 +0100

    [KBM-227][UPDATE] The settings button css

--- src/components/SettingsButton.tsx
+++ src/components/SettingsButton.tsx
@@ -13,9 +13,9 @@ const SettingButton: React.FC = () => {
 
   return (
     <button
-      className={'relative px-28 py-8 text-2xl font-bold text-gray-100 z-55'}
+      className={'relative p-10 text-2xl font-bold text-gray-100 brightness-70'}
       style={{
-        backgroundImage: 'url(/images/settings-button.webp)',
+        backgroundImage: 'url(/images/settings-icon.webp)',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
