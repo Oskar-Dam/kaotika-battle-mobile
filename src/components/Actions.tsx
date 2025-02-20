4abccf5df5bd2e4fa89071abbc817a606ed2b85f
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-20 08:22:22 +0100

    [KBM-STAGING][UPDATE] SettingButton removed from rendering

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { Potion } from '../interfaces/Potion';
-import useStore from '../store/useStore';
 import AttackButton from './AttackButton';
 import PotionContainer from './PotionContainer';
-import SettingButton from './SettingsButton';
 
 interface ActionsProps {
   openPotionModal: (potion: Potion) => void;
 }
 
 const Actions: React.FC<ActionsProps> = ({openPotionModal}) => {
-  const {
-    isSettingModalOpen,
-  } = useStore();
+  
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-[49%] border-0 border-yellow-400">
       <AttackButton/>
-      { !isSettingModalOpen && (
+      {/* { !isSettingModalOpen && (
         <div className='w-full flex items-center place-self-center justify-center mt-[5%]'>
           <SettingButton />
         </div>
-      )};
+      )}; */}
       
       {/* )}       */}
       <div className='w-full flex items-center justify-center m-[10%]'>
