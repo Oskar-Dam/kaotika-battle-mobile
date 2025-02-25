5d925c71793b57ed70f09fd1b5a4624f0ea10fe1
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-25 09:41:52 +0100

    [KBM-231][UPDATE] AdminScreen and BattleList

--- src/components/AdminScreen.tsx
+++ src/components/AdminScreen.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import BattleList from './BattleList';
 import WelcomeTexts from './WelcomeTexts';
+import JoinButton from './JoinButton';
 
 const AdminScreen: React.FC = () => {
 
   return (
-    <div className='flex h-screen w-screen'>
+    <div
+      className='flex h-screen w-screen'
+      style={{backgroundImage: 'url(/images/background/adminScreenBG.webp)', backgroundSize: '100% 100%'}}>
 
-      <div className='flex flex-col justify-start items-center h-full w-full '>
-        <div className='text-black'>HELLO</div>
-        <WelcomeTexts/>
-        <BattleList />
+      <div className='flex flex-col justify-start items-center h-full w-full'>
+        <div className='flex h-[20%] bg-yellow-400 mb-5'>
+          <WelcomeTexts/>
+        </div>
+        <div className='flex h-[60%] w-[95%] mb-5'>
+          <BattleList/>
+        </div>
+        <div className='flex h-[10%] w-[80%]'>
+          <JoinButton/>
+        </div>
       </div>
     </div>
   );
--- src/components/BattleList.tsx
+++ src/components/BattleList.tsx
@@ -4,7 +4,8 @@ import mockBattles from '../__mocks__/mockBattles';
 
 const BattleList: React.FC = () => {
   return (
-    <div className='flex flex-col h-[35%] w-full items-center overflow-y-auto mt-10'>
+    <div className='flex flex-col h-[100%] w-full items-center overflow-y-auto scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100 border border-gray-300 rounded-lg'>
+      <div className='flex w-full items-center text-white justify-center text-4xl rounded-2xl'>BATTLES</div>
       {mockBattles.map((battle, index) => (
         <div
           key={index}
