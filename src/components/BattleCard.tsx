5ea099987fc72f8464f5f7c5b91f336463eba26c
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-25 10:16:49 +0100

    [KBM-231][UPDATE] BattleList and BattleCard

--- src/components/BattleCard.tsx
+++ src/components/BattleCard.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 interface BattleCardProps {
   suggested_level: number;
   name: string;
+  index: number;
 }
 
-const BattleCard: React.FC<BattleCardProps> = ({ suggested_level, name }) => {
+const BattleCard: React.FC<BattleCardProps> = ({ suggested_level, name, index }) => {
   return (
-    <div className='flex flex-row w-[100%] h-[10vh] justify-center items-center my-1 border-2 rounded border-white'>
+    <button 
+      className='flex flex-row w-[100%] h-[13vh] justify-center items-center border-2 rounded border-white'
+      onClick={() => console.log('Battle card clicked ' + index)}>
       <div className='flex w-[30%] h-full items-center justify-center mr-1'>
         <h1 className='text-4xl text-white'>Lv. {suggested_level}</h1>
       </div>
@@ -16,7 +19,7 @@ const BattleCard: React.FC<BattleCardProps> = ({ suggested_level, name }) => {
       <div className='flex w-[75%] h-full items-center justify-center'>
         <h1 className='text-3xl text-white'>{name}</h1>
       </div>
-    </div>
+    </button>
   );
 };
 
--- src/components/BattleCardSeparator.tsx
+++ src/components/BattleCardSeparator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BattleCardSeparator: React.FC = () => {
   return (
-    <div className='flex flex-col w-[100%]'>
+    <div className='flex flex-col w-[100%] h-[8%]'>
       {/* Separator content */}
     </div>
   );
