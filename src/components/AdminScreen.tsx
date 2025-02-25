0acfc7fed8f94cc09ff7e0386036ab9a8ee31467
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-25 09:12:03 +0100

    [KBM-231][ADD] Background image for admin screen

--- src/components/AdminScreen.tsx
+++ src/components/AdminScreen.tsx
@@ -6,7 +6,9 @@ const AdminScreen: React.FC = () => {
 
   return (
     <div className='flex h-screen w-screen'>
+
       <div className='flex flex-col justify-start items-center h-full w-full '>
+        <div className='text-black'>HELLO</div>
         <WelcomeTexts/>
         <BattleList />
       </div>
