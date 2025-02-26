c38826cd7538e8b1ceeee26e8093eac3904b6da1
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-26 12:58:56 +0100

    [KBM-265][UPDATE] LoginFirebase spinner and acolytelobbyscreen styles

--- src/pages/AcolyteLobbyScreen.tsx
+++ src/pages/AcolyteLobbyScreen.tsx
@@ -19,24 +19,15 @@ const AcolyteLobby: React.FC = () => {
       style={{ backgroundColor: 'black', backgroundRepeat:'no-repeat',backgroundPositionX:'50%',backgroundPositionY:'35%' ,backgroundImage: 'url(/images/acolyt-lobby-screen.webp)', backgroundSize: '100% 100%' }}
       data-testid="acolyte-lobby-screen"
     >
+      
       <div className="flex items-center justify-center w-[80%] py-14 h-1/8 text-8xl text-white bg-black/50 rounded-lg">Lobby</div>
-      <div className="h-1/8" />
-      <div className="flex items-center justify-center w-full h-2/8">
+      <div className="h-2/8" />
+      <div className="flex items-center justify-center w-full h-1/8">
         <JoinButton/>
       </div>
       <div className="h-1/8" />
-      <div className="flex items-center justify-center w-full h-2/8">
-        <button
-          className="w-full h-full bg-black text-white text-5xl rounded-4xl shadow-black shadow-xl border-2 border-white"
-          onClick={() => {
-            console.log('???');
-          }}
-        >
-            Comming soon...
-        </button>
-      </div>
-      <div className="h-1/8" />
-      <div className="flex items-center justify-center w-full h-2/8">
+
+      <div className="flex items-center justify-center w-full h-1/8">
         <ReturnToModeSelectionScreenButton/>
       </div>
       <div className="h-1/8" />
