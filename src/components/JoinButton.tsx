5fd97b714346eab4f79910a3b267d8ce5e639c34
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-26 10:53:04 +0100

    [KBM-257][UPDATE] JoinBattle button color depending on isGameCreated

--- src/components/JoinButton.tsx
+++ src/components/JoinButton.tsx
@@ -5,22 +5,22 @@ import useStore from '../store/useStore';
 
 const JoinButton: React.FC = () => {
 
-  const { player ,gameCreated } = useStore();
+  const { player, gameCreated } = useStore();
   const joinBattle = () => {
     socket.emit(SOCKET_EMIT_EVENTS.JOIN_BATTLE, player._id);
     console.log('sended join battle socket');
   };
-
+  
   return (
     <button
       onClick={() => { 
         joinBattle(); 
       }}
-      className="w-full h-full bg-black text-white text-5xl rounded-4xl shadow-black shadow-xl border-2 border-white"
+      className={`w-full h-full bg-black text-white text-5xl rounded-4xl shadow-black shadow-xl border-2 ${gameCreated ? 'border-green-500' : 'border-red-500'}`}
       disabled={!gameCreated}
       aria-disabled={!gameCreated}
     >
-      JOIN
+      <span className={gameCreated ? 'text-green-500' : 'text-red-500'}>JOIN</span>
     </button>
   );
 };
