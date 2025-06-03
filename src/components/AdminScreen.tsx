dbc276cb849a8a710254770b928129a228703088
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-06-03 09:30:49 +0200

    [UPDATE] Sockets logs. [UPDATE] AdminScreen with Join and Start buttons

--- src/components/AdminScreen.tsx
+++ src/components/AdminScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MobileJoinBattleResponse } from '../interfaces/JoinBattleReponse';
 import { SOCKET_EMIT_EVENTS } from '../sockets/events';
 import socket from '../sockets/socket';
@@ -8,7 +8,9 @@ import BattleList from './battles/BattleList';
 import MenuButton from './MenuButton';
 
 const AdminScreen: React.FC = () => {
-  const { setGameJoined, gameCreated, player, gameStarted, setIsBattleSelected, setIsAdventureSelected, setGameCreated } = useStore();
+  const { setGameJoined, gameCreated, player, gameStarted, setIsBattleSelected, setIsAdventureSelected, setGameCreated, setGameEnded } = useStore();
+
+  const [resetEffect, setResetEffect] = useState(false);
 
   const joinBattle = () => {
     socket.emit(SOCKET_EMIT_EVENTS.JOIN_BATTLE, player._id, (response: MobileJoinBattleResponse) => {
@@ -42,7 +44,14 @@ const AdminScreen: React.FC = () => {
       // Limpieza de listeners si es necesario
       socket.off(SOCKET_EMIT_EVENTS.GAME_CREATED);
     };
-  }, [setGameCreated]);
+  }, [resetEffect, setGameCreated]);
+
+  const handleStartGame = (): void => {
+    console.log('Game start button pressed');
+    socket.emit(SOCKET_EMIT_EVENTS.GAME_START);
+    setResetEffect(prev => !prev);
+    setGameEnded(false);
+  };
 
   return (
     <div
@@ -54,17 +63,32 @@ const AdminScreen: React.FC = () => {
           <BattleList />
         </div>
         <div className="flex h-[10%] w-[90%] mb-2">
-          <MenuButton
-            text="JOIN"
-            onClick={joinBattle}
-            disabled={!gameCreated || gameStarted}
-            ariaDisabled={!gameCreated || gameStarted}
-            extraStyles={
-              !gameCreated || gameStarted
-                ? 'text-red-500 border-red-500'
-                : 'text-green-500 border-green-500'
-            }
-          />
+          <div className='flex w-[50%] mr-1'>
+            <MenuButton
+              text="JOIN"
+              onClick={joinBattle}
+              disabled={!gameCreated || gameStarted}
+              ariaDisabled={!gameCreated || gameStarted}
+              extraStyles={
+                !gameCreated || gameStarted
+                  ? 'text-red-500 border-red-500'
+                  : 'text-green-500 border-green-500'
+              }
+            />
+          </div>
+          <div className='flex w-[50%] ml-1'>
+            <MenuButton
+              text="START"
+              onClick={handleStartGame}
+              disabled={!gameCreated || gameStarted}
+              ariaDisabled={false}
+              extraStyles={
+                !gameStarted && gameCreated
+                  ? 'text-green-500 border-green-500'
+                  : 'text-red-500 border-red-500'
+              }
+            />
+          </div>
         </div>
         <div className="flex h-[10%] w-[90%] mt-2">
           <MenuButton
